1102f25ba87787dca9379a684111afdb0b7ed3a6
zelda van der Waal <zeldavdwaal@gmail.com>
2026-02-13 13:40:35 +0000

    app_password
    from .Renviron
    app_password <- Sys.getenv("BEAVERAPP_PASSWORD")

--- app.R
+++ app.R
@@ -1,8 +1,15 @@
-# This is a Shiny web application to run the real package (beaverApp) from GitHub.
+# This is a Shiny web application to run the real package (beaverApp) from GitHub, along with custom regional data (also on gitHub)
+
 
 #  the demo data backend 
 options(beaverApp.data.pkg = "beaverAppDataEngland")   #  data package for this deployment 
-Sys.setenv(BEAVERAPP_DATA_PKG = "beaverAppDataEngland", APP_MODE="England") 
+Sys.setenv(BEAVERAPP_DATA_PKG = "beaverAppDataEngland", 
+           APP_MODE="normal",    
+           BEAVERAPP_VERBOSE =FALSE,
+           APP_REGION= "England")
+           # BEAVERAPP_PASSWORD=  XXX  
+
+app_password <- Sys.getenv("BEAVERAPP_PASSWORD") 
 
 options(shiny.maxRequestSize = 7200 * 1024^2)  # just in case, 7200MB uploads
 options(rsconnect.max.bundle.size=3145728000)
@@ -12,11 +19,5 @@ if (!is.na(Sys.getenv("SHINY_PORT", unset = NA))) {
   future::plan("sequential")
 }
 
-cat("\n>>> launching beaverApp from wrapper (England) >>>\n")
-#library(beaverApp) 
-beaverApp::launch_app(run = FALSE)  
-# return a shiny.appobj for shinyapps.io to run.
-# For shinyapps.io, the last expression in app.R must evaluate to the app object. 
-   
-
-
+cat("\n>>> launching beaverApp from England wrapper >>>\n") 
+beaverApp::launch_app(run = FALSE) 
